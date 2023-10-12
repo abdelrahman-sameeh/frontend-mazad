98ab6a124edd171190fb34aac89b8508997ace70
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2023-10-12 09:56:47 +0300

    for socket io

--- src/customHooks/sockets/InitialSocket.js
+++ src/customHooks/sockets/InitialSocket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
-export const socket = io.connect(`http://localhost:3001`);
+// export const socket = io.connect(`http://localhost:3001`);
+export const socket = io.connect(`https://api-mazad-nodejs.onrender.com`);
+
 
 const InitialSocket = () => {
   const handleGoToMazadChat = (chatId) => {
